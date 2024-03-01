d0f1c8380a0c1311089f70eb99119c3314d52937
Eddie Molina <eddiemolina@Eddies-MBP.lan>
2024-03-01 14:31:46 -0500

    cleaning up javascript

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ let generateButton = document.getElementById("generateButton");
 let code = document.getElementById("Code");
 
 // variables 
-
 const hexKeyCode = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 
 // functions
-
-// let testing = () => {
-//     console.log("click")
-// };
-
+// new background function
 let generateNew = () => {
     let hexCode = "#";
 
@@ -25,13 +20,10 @@ let generateNew = () => {
     code.innerHTML = hexCode;
     console.log(hexCode);
 };
-
+// copy button function 
 let copyContent = () => {
     navigator.clipboard.writeText(code.innerHTML);
 };
-
-
-
+// click prompts
 generateButton.addEventListener("click", generateNew);
 copyButton.addEventListener("click", copyContent);
-
